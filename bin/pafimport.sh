ddfe498cf75ca614e563fb42caa19d68011a2085
Mike Lippert <mlippert255@gmail.com>
2013-12-11 17:27:38 -0500

    PAF import script to import the MCQ (and other) activities and assignments

---
+++ bin/pafimport.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+PAFIMPORTER='java -jar brix-tool-pafclient-0.2-jar-with-dependencies.jar'
+METHOD=POST
+URL='http://repo.paf.cert.pearsoncmg.com/paf-repo/resources/activities'
+ACTIVITY_HDR='Content-Type: application/vnd.pearson.paf.v1.envelope+json;body="application/vnd.pearson.sanvan.v1.activity""'
+ASSIGNMENT_HDR='Content-Type: application/vnd.pearson.paf.v1.envelope+json;body="application/vnd.pearson.paf.v1.assignment+json""'
+
+if [[ $1 == 'activity' ]]
+then
+    HEADER=$ACTIVITY_HDR
+    shift
+elif [[ $1 == 'assignment' ]] 
+then
+    HEADER=$ASSIGNMENT_HDR
+    shift
+else
+    echo "The first argument must be either 'activity' or 'assignment'"
+    exit 1
+fi
+
+for jsonfile
+do
+    echo importing $jsonfile to PAF...
+    $PAFIMPORTER -c -m $METHOD -u $URL -h "$HEADER" -d "$jsonfile"
+    echo ...done importing $jsonfile to PAF
+done
