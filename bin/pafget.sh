3fec93f79ec12f622df70844a02408220449525e
Mike Lippert <mlippert255@gmail.com>
2013-12-16 10:23:47 -0500

    minor tweaks to pafget

--- bin/pafget.sh
+++ bin/pafget.sh
@@ -3,8 +3,8 @@
 # get an activity: ./pafget.sh activity PUT_THE_GUID_HERE
 # get an assignment: ./pafget.sh assignment PUT_THE_GUID_HERE
 
-PAFIMPORTER='java -jar brix-tool-pafclient-0.2-jar-with-dependencies.jar'
-METHOD=POST
+PAFIMPORTER='java -jar brix-tool-pafclient-0.3-jar-with-dependencies.jar'
+METHOD=GET
 URL='http://repo.paf.cert.pearsoncmg.com/paf-repo/resources/activities'
 ACTIVITY_HDR='Accept:application/vnd.pearson.sanvan.v1.activity'
 ASSIGNMENT_HDR='Accept:application/vnd.pearson.paf.v1.assignment+json'
@@ -14,21 +14,21 @@ GUID=$2
 if [[ $TYPE == 'activity' ]]
 then
     HEADER=$ACTIVITY_HDR
-    shift
 elif [[ $TYPE == 'assignment' ]] 
 then
     HEADER=$ASSIGNMENT_HDR
-    shift
 else
     echo "The first argument must be either 'activity' or 'assignment'"
+    echo ''
+    echo 'usage:'
+    echo 'get an activity:   ./pafget.sh activity PUT_THE_GUID_HERE'
+    echo 'get an assignment: ./pafget.sh assignment PUT_THE_GUID_HERE'
+    echo ''
     exit 1
 fi
 
 date
-for jsonfile
-do
-    echo getting $TYPE $GUID from PAF cert...
-    $PAFIMPORTER -c -m GET -h "$HEADER" -u $URL/$GUID
-    echo ...done getting $TYPE $GUID from PAF cert
-	date
-done
+echo getting $TYPE $GUID from PAF cert...
+$PAFIMPORTER -c -m $METHOD -h "$HEADER" -u $URL/$GUID
+echo ...done getting $TYPE $GUID from PAF cert
+date
